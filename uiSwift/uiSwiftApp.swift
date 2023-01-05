f763e3c990f4bf4c2d9184ab4689debdb97cb375
박형주 <hyeongju1122@gmail.com>
2023-01-05 10:13:06 +0900

    Initial Commit

---
+++ uiSwift/uiSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiSwiftApp.swift
+//  uiSwift
+//
+//  Created by 박형주 on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct uiSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
